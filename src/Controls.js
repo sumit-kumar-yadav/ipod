e751a05f2de4bc25c8c4a3af190c71925f640190
Sumit <sumityadav0376@gmail.com>
2021-05-10 13:34:06 +0530

    Design wheel UI

--- src/Controls.js
+++ src/Controls.js
@@ -3,8 +3,12 @@ import './Controls.css'
 function Controls() {
     return (
       <section id="controls">
-        <div id="buttons-container">
-          {/* menu, left, right, fwd icons */}
+        <div id="wheel">
+          {/* menu, left, right, fwd */}
+          <span className="buttons" style={ {top: 20} }>Menu</span>
+          <img className="buttons" src="https://image.flaticon.com/icons/png/128/2404/2404395.png" alt="left" style={ {left: 13, width: 40} }></img>
+          <img className="buttons" src="https://image.flaticon.com/icons/png/128/2404/2404393.png" alt="right" style={ {right: 13, width: 40} }></img>
+          <img className="buttons" src="https://image.flaticon.com/icons/png/128/26/26810.png" alt="bottom" style={ {bottom: 13, width: 30, height: 37} }></img>
           <div id="ok-button">OK</div>
         </div>
       </section>
